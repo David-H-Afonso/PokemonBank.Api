20426b9cb34fd784ac7ae23ab3a9be35c1edeef5
David Hormiga Afonso <davidhormigafonso@gmail.com>
2025-08-15 15:19:21 +0200

    Add OriginalFileName column to Files table for enhanced file tracking

---
+++ Migrations/20250815120635_AddOriginalFileNameToFiles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonBank.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOriginalFileNameToFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalFileName",
+                table: "Files",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalFileName",
+                table: "Files");
+        }
+    }
+}
